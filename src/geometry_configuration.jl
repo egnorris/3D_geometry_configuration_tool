7a26cbfaadcd7ba15b9c065f24dcb60294026a1c
egnorris <evangnorris@gmail.com>
2023-08-29 13:28:03 -0400

    added functions to module for documentation

--- src/geometry_configuration.jl
+++ src/geometry_configuration.jl
@@ -1,20 +1,82 @@
 module geometry_configuration
 using JSON, GLMakie
 
-export load_geometry
+export get_vertices, set_plane, rectangle_vertices,  triangle_vertices, circle_vertices, place_shape. load_JSON
 
-""""
-    load_geometry()
+function load_JSON()
+    return [open(JSON.parse, "geometry.json"), open(JSON.parse, "configuration.json")]
+end
 
-loads in the geometry defined in the geometry.json file
-"""
-function load_geometry()
-    geometry = []
-    D = JSON.parsefile("geometry.json")
-    for i = 1:length(D)
-        push!(geometry, D[i])
+function get_vertices(x1_coordinate_slider, x2_coordinate_slider, plane, L, W, T, R, shape)
+    if shape == "Rectangle"
+        return rectangle_vertices(x1_coordinate_slider, x2_coordinate_slider, plane, L, W, T)
+    elseif shape == "Triangle"
+        if plane == "xz"
+            #triangles have to be placed with face in the xz plane
+            return triangle_vertices(x1_coordinate_slider, x2_coordinate_slider, W, L)
+        else
+            return rectangle_vertices(x1_coordinate_slider, x2_coordinate_slider, plane, L, W, T)
+        end
+    elseif shape == "Rod"
+        if plane == "xz"
+            #triangles have to be placed with face in the xz plane
+            return circle_vertices(x1_coordinate_slider, x2_coordinate_slider, R) 
+        else
+            return rectangle_vertices(x1_coordinate_slider, x2_coordinate_slider, plane, L, W, T)
+        end
     end
-    return geometry
+end
+
+function set_plane(plane, L, W, T)
+    if plane == "xz"
+        return [W, L]
+    elseif plane == "xy"
+        return [W, T]
+    elseif plane == "yz"
+        return [T, L]
+    end
+end
+
+function rectangle_vertices(x1_coordinate_slider, x2_coordinate_slider, plane, L, W, T)
+    dim1, dim2 = set_plane(plane, L,W,T)
+    vertices = lift(x1_coordinate_slider.value, x2_coordinate_slider.value) do x1, x2
+        [
+            Point2f(x1 + dim1, x2 + dim2),
+            Point2f(x1 + dim1, x2 - dim2),
+            Point2f(x1 - dim1, x2 - dim2),
+            Point2f(x1 - dim1, x2 + dim2)
+        ]
+    end
+    return vertices
+end
+    
+function triangle_vertices(x1_coordinate_slider, x2_coordinate_slider, W, L)
+    vertices = lift(x1_coordinate_slider.value, x2_coordinate_slider.value) do x1, x2
+        [
+            Point2f(x1 + W, x2 + L),
+            Point2f(x1 + W, x2 - L),
+            Point2f(x1 - W, x2 - L),
+            Point2f(x1 - W, x2 + L)
+        ]
+    end
+    return vertices
+end
+
+function circle_vertices(x1_coordinate_slider, x2_coordinate_slider, R) 
+        vertices = lift(x1_coordinate_slider.value, x2_coordinate_slider.value) do x1, x2
+            Circle(Point2f(x1, x2), R)
+        end
+    return vertices
+end
+
+function place_shape(x1_coordinate_slider, x2_coordinate_slider, plane, L, W, T, R, shape)
+    center_coordinates = lift(x1_coordinate_slider.value, x2_coordinate_slider.value) do x1, x2
+        Point2f(x1, x2)
+    end
+    vertice_coordinates = get_vertices(x1_coordinate_slider, x2_coordinate_slider, plane, L, W, T, R, shape)
+    x1_coordinate_string = lift(x1 -> "$x1", x1_coordinate_slider.value)
+    x2_coordinate_string = lift(x2 -> "$x2", x2_coordinate_slider.value)
+    return [center_coordinates, vertice_coordinates, x1_coordinate_string, x2_coordinate_string]
 end
 
 end
